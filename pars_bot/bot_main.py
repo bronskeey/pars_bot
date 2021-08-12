4e99886ac77f2596addb66ceaf4ec036134a4275
bronskeey <ksespeer@gmail.com>
2021-08-12 14:03:48 +0300

    Бот + Парсер v0.02
    1. Добавлено:
    
    bot_main.py
    1) После выполнения парсера бот теперь отправляет сами файлы, а не текст.
    2) Бот добавлен в группу, в которую присылает результат работы парсера. В группе бот может быть запущен только владельцем.
    3) Бот теперь присылает "No new updates" сообщения в тихом режиме.
    4) Сообщения "No new updates" теперь содержат информацию о том, на какой странице был выполнен парсинг(список страниц и их обозначения в самом конце).
    
    2. Исправлено:
    
    pars_main.py
    1) Исправлено логирование в связи с введением новых обозначений.
    2) Получение имён в 'download_files'.
    
    3. Убрано:
    6) Из бота убрана масса лишних функций, таких как большие кнопки, команда 'help'(временно) и т.д.
    
    4. Прочие минорные изменения.
    
    5. Список страниц и их обозначения в логах:
    * пункты 3 и 4, как и обозначения 'info' и 'meth' добавлены в v0.02
    1) [gia-11] [info] - 'http://rcoi.mcko.ru/organizers/info/gia-11/'
    2) [gia-9] [info]  - 'http://rcoi.mcko.ru/organizers/info/gia-9/'
    3) [ege] [meth]    - 'http://rcoi.mcko.ru/organizers/methodological-materials/ege/'
    4) [gia-9] [meth]  - 'http://rcoi.mcko.ru/organizers/methodological-materials/gia-9/'

--- pars_bot/bot_main.py
+++ pars_bot/bot_main.py
@@ -15,152 +15,99 @@ bot    = telebot.TeleBot(token)
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG) 
 my_id  = '107579649'
-users_random = {'65535': '2021-05-05'}
-urls = ['http://rcoi.mcko.ru/organizers/info/gia-11/','http://rcoi.mcko.ru/organizers/info/gia-9/']
+urls_organizers = ['http://rcoi.mcko.ru/organizers/info/gia-11/', 'http://rcoi.mcko.ru/organizers/info/gia-9/'] #
+urls_methodolog = ['http://rcoi.mcko.ru/organizers/methodological-materials/ege/', 'http://rcoi.mcko.ru/organizers/methodological-materials/gia-9/']
+urls = urls_organizers + urls_methodolog
 
-
-button_random_text = f'Random number'
-button_scrap_text  = f'Get scrap info'
-button_back_text   = f'Back to main menu'
-
-    
-
+# REPEAT PARSE EVERY 'P_HOUR' HOURS
+P_HOUR = 24
 
 @bot.message_handler(commands=['start'])
-def start_f(message):
+def start_command(message):
     """
-    start
+     - calling 'welcome' function for the list of commands
     """
-    welcome(message.from_user.id, 0, False)
+    welcome(message.chat.id, 0)
 
-
-def welcome(id_c, id_m, edit_flag):
+def welcome(id_c, id_m):
     start_string =\
         f"/start - Show main menu\n"+\
-        f"/help  - Some info about\n"+\
+        f"/pars  - Start RCOI parser\n"+\
         f"/random - Get a random number"
-    
     start_markup    = types.InlineKeyboardMarkup(row_width=2)
-    button_big      = types.InlineKeyboardButton(text='Show me big keys', callback_data='start_button_big')
     button_feedback = types.InlineKeyboardButton(text='Feedback', callback_data='start_button_feedback')
-    button_secret   = types.InlineKeyboardButton(text='Secret', callback_data='start_button_secret')
-    
     start_markup.add(
-        button_big, 
-        button_feedback,  
-        button_secret)  
-    
-    if edit_flag:
-        bot.edit_message_text(chat_id=id_c, message_id=id_m, text=start_string, reply_markup=start_markup)
+        button_feedback)
+    bot.send_message(id_c, text = start_string, reply_markup = start_markup)
+
+
+@bot.message_handler(commands=['pars'])
+def pars_command(message):
+    if message.from_user.id == int(my_id):
+        bot.send_message(message.chat.id, text='='*5 + 'Parser started' + '='*5, disable_notification=True)
+        start_pars(message)
+        bot.send_message(message.chat.id, text='='*6 + 'Parser ended' + '='*6, disable_notification=True)
     else:
-        bot.send_message(id_c,                  
-                     text = start_string,        
-                     reply_markup = start_markup)
-      
-    
-@bot.message_handler(commands=['help'])
-def help_f(message):
-    text = 'help'
-    bot.reply_to(message,
-                 text)
-
-
-@bot.message_handler(commands=['random'])
-def random_f(message):
-    random_text = get_random()
-    bot.send_message(message.chat.id,text = random_text)
-
-
-@bot.message_handler(content_types=['text'])
-def text_f(message):
-    """
-    plain text handler, 'привет' is the only option
-    """
-    if message.chat.type == 'private':
-
-        if message.text == 'привет':
-            bot.send_message(message.from_user.id,
-                             'И тебе привет!')
-
-        elif message.text == button_random_text:
-            random_text = get_random()
-            bot.send_message(message.chat.id,text = random_text)
-    
-        elif message.text == button_scrap_text:
-            try:
-                start_pars(message)
-            except Exception as e:
-                print(e)
-            # for url in urls:
-            #     pars_object = pars_rcoi(url)
-            #     pars_object.get_info()
-            #     status = pars_object.bot_status
-            #     bot.send_message(message.chat.id,text = status)
-
-        elif message.text == button_back_text:
-            hide_big_markup      = types.ReplyKeyboardRemove(selective=True)
-            big_buttons_off_text = f'--Buttons off--'
-            bot.send_message(message.chat.id, 
-                             big_buttons_off_text, 
-                             reply_markup = hide_big_markup)
-            welcome(message.chat.id,0,False)
-
-def test_function(message):
-    empty_text = '*' * 25 + \
-     '\nNo new updates!\n' + \
-     '*' * 25
-    t = None
-    status = empty_text if t == '' else 7
-    bot.send_message(message.chat.id,text = status)
-    threading.Timer(10, test_function, args=[message,]).start()
+        bot.reply_to(message, text="Sorry, you don't have enough rigths to do this!")
+        print(f'{message.from_user.id} tried to start the parser')
 
 def start_pars(message):
-    empty_text = '*' * 25 + \
-                 '\nNo new updates!\n' + \
-                 '*' * 25
-
     for url in urls:
         pars_object = pars_rcoi(url)
         pars_object.get_info()
-        status = empty_text if pars_object.bot_status == '' else pars_object.bot_status
-        bot.send_message(message.chat.id,text = status)
+        if pars_object.bot_status == '':
+            empty_text = '*' * 25 + \
+                         f'\n{pars_object.MAIN_TYPE}\n' + \
+                         f'\n{pars_object.MAIN_NAME}\n' + \
+                         '\nNo new updates!\n' + \
+                         '*' * 25
+            status = empty_text
+            bot.send_message(message.chat.id, text=status, disable_notification=True)
+        else:
+            status    = pars_object.bot_status
+            bot.send_message(message.chat.id, text=status)
+            data_dir  = f'./{pars_object.last_dir}/'
+            file_list = os.listdir(data_dir)
+            for file in file_list:
+                try:
+                    with open(data_dir + file, 'rb') as f:
+                        bot.send_document(message.chat.id, f)
+                except PermissionError as e:
+                    bot.send_message(message.chat.id, text="Something went wrong. I can't send you the files.")
+                    print(e)
+                    print(data_dir)
+                    print(file_list)
 
-    threading.Timer(24*60*60, start_pars, args=[message,]).start()
-        
+    threading.Timer(P_HOUR * 60 * 60, start_pars, args=[message, ]).start()
+
+
+@bot.message_handler(commands=['random'])
+def random_command(message):
+    """
+     - getting random number from 'get_random' function,
+       sending it back to chat.
+    """
+    random_text = get_random()
+    bot.send_message(message.chat.id,text = random_text)
+
 def get_random():
     random_integer     = random.randint(0, 100)
-    string =  f'Random number: {random_integer}'  
+    string =  f'Random number: {random_integer}'
     return string
-            
+
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('start_button'))
 def call_start_buttons(call):
-    if call.data   == "start_button_big": 
-        big_markup    = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)# на сколько наполнять ряд
-        button_first  = types.KeyboardButton(button_random_text)
-        button_second = types.KeyboardButton(button_scrap_text)
-        button_back   = types.KeyboardButton(button_back_text)
-        
-        big_markup.add(
-            button_first,
-            button_second,
-            button_back)
-        
-        big_buttons_on_text = f'--Buttons on--'
-        
-        bot.send_message(call.message.chat.id,
-                         text = big_buttons_on_text,
-                         reply_markup = big_markup)
-        
-    elif call.data == "start_button_feedback":
+    """
+     - handling the only call for feedback button
+     - calling 'save_csv' to save feedback messages to 'data/feedback.csv'
+    """
+    if call.data == "start_button_feedback":
         keyboard_markup = types.ForceReply(selective=False)
         msg             = bot.send_message(call.message.chat.id,
                                              'Оставьте свой отзыв! ',
                                              reply_markup = keyboard_markup)
         bot.register_next_step_handler(msg, save_csv)
-
-
-
 
 def save_csv(message):
     file_exists  = os.path.isfile('data/feedback.csv')
@@ -191,7 +138,12 @@ def save_csv(message):
         bot.send_message(message.from_user.id, f'Thank you!')
 
 
-
+# def test_function(message):
+#     empty_text = '*' * 25 + \
+#      '\nNo new updates!\n' + \
+#      '*' * 25
+#     t = None
+#     status = empty_text if t == '' else 7
+#     bot.send_message(message.chat.id,text = status)
+#     threading.Timer(10, test_function, args=[message,]).start()
 bot.polling(none_stop=True)
-
-
--- pars_bot/pars_main.py
+++ pars_bot/pars_main.py
@@ -1,29 +1,30 @@
 # coding: utf-8
+import inspect
+
 from bs4 import BeautifulSoup
 from random import uniform, randint
+from datetime import date
 import requests
 import os.path
 import time
 import ast
-'''
-20.07: maybe it is working
-	   changelog:
-	   		- comparing dicts to check if there's a file before LM
-	   		 
+from tqdm import tqdm
 
+# TODO: do something with functions inside of a function
 
-'''
 class pars_rcoi(object):
-	def __init__(self, url):
-		self.url = url
-		self.MAIN_NAME = url.split('/')[-2]
+	def __init__(self, url_):
+		self.url = url_
+		self.MAIN_NAME = url_.split('/')[-2]
+		self.MAIN_TYPE = url_.split('/')[-3][:4]
 		self.docs_list = []
 		self.docs_links = []
 		self.docs_og_names = []
 		self.docs_last_m = dict()
-		self.BAD_EXTENSIONS = ['wav','mp3']
+		self.BAD_EXTENSIONS = ['wav','mp3', 'ru']
 		self.bot_status = ''
 		self.soup_div   = None
+		self.last_dir = ''
 
 
 	def get_info(self):
@@ -34,102 +35,89 @@ class pars_rcoi(object):
 		   so I won't check 'last-modified' for 'BAD_EXTENSIONS' files
 		   bc RCOI won't modify music files
 		- maybe '/n' replacing is not needed at the end
+		- redo some things on small pages, maybe change link analysis
 		"""
 
-		print(f'[{self.MAIN_NAME}][LOG][IN] Getting soup ready')
+		print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN]  {inspect.stack()[0][3]}')
 		response      = requests.get(self.url)
 		soup          = BeautifulSoup(response.text, 'lxml')
 		self.soup_div = soup.find('div', class_ = 'typicaltext')
 		div_ps        = self.soup_div.find_all('p')
 		div_uls       = self.soup_div.find_all('li')
-
-		for raw_link in div_ps+div_uls:
+		for raw_link in tqdm(div_ps+div_uls):
 			link_A           = raw_link.find_all('a')
 			doc_name         = raw_link.text.strip()
 			link_descendants = [desc.name for desc in raw_link.descendants if desc.name is not None]
-
-			if (len(link_A) == 1 and 
-			'a' in link_descendants and 
-			doc_name not in self.docs_list):
+			if (len(link_A) == 1               and
+				'a' in link_descendants        and
+				doc_name not in self.docs_list and
+				"dwnico" in str(link_A)):
 
 				link_href      = link_A[0].get('href')
 				link_full_url  = 'http://rcoi.mcko.ru' + link_href
 				link_file_name = link_href.split('/')[-1].strip()
 				link_file_ext  = link_file_name.split('.')[-1]
-
 				if link_file_ext not in self.BAD_EXTENSIONS:
-					full_link_response = requests.get(link_full_url) 
+					full_link_response = requests.get(link_full_url)
 					link_last_modified = full_link_response.headers.get('Last-Modified', False)
 				else:
 					link_last_modified = 0
-
 				self.docs_list.append(doc_name)
 				self.docs_links.append(link_full_url.strip())
-				self.docs_og_names.append(link_file_name) 
+				self.docs_og_names.append(link_file_name)
 				self.docs_last_m[link_file_name] = link_last_modified
-			
 		self.docs_list = [doc.replace('\n','') for doc in self.docs_list]
+		print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] {inspect.stack()[0][3]}')
 
 		self.new_files()
 		self.changed_files()
 
-		# self.new_files()
-		# self.changed_files()
 
 	def new_files(self):
+
 		def main(self):
-			# dump_file_list(self)
 			diff_indexes = check_new_files(self)
 			if diff_indexes:
 				self.download_files(diff_indexes)
 				dump_file_list(self)
 
 		def check_new_files(self):
-			print(f'[{self.MAIN_NAME}][LOG][IN] Checking new files..')
-
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN] {inspect.stack()[0][3]}')
 			prev_file_list = get_previous_file_list(self)
 			diff           = [x for x in self.docs_list if x not in prev_file_list]
 			indexes        = [self.docs_list.index(d) for d in diff]
-
 			if diff:
-				print(f'[{self.MAIN_NAME}][LOG][OUT] Found new files!')
+				print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] Found new files!')
 			else:
-				print(f'[{self.MAIN_NAME}][LOG][OUT] No new files!')
-
+				print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] No new files!')
 			return indexes
 
 		def get_previous_file_list(self):
-			print(f'[{self.MAIN_NAME}][LOG][IN] Checking previous file list..')
-
-			PATH      = 'file_lists/' + self.MAIN_NAME + '.txt'
-
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN] {inspect.stack()[0][3]}')
+			PATH      = f'./file_lists/{self.MAIN_TYPE}_{self.MAIN_NAME}.txt'
 			if not os.path.exists(PATH):
-				print(f'[{self.MAIN_NAME}][LOG][INFO] Documents list not found!')
-				print(f'[{self.MAIN_NAME}][LOG][INFO] Creating a documents list..')
+				print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [INFO] Documents list not found!')
+				print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [INFO] Creating a documents list..')
 				dump_file_list(self)
-
-			txt_file  = open('file_lists/' + self.MAIN_NAME + '.txt', 'r', encoding="utf-8")
+			txt_file  = open(PATH, 'r', encoding="utf-8")
 			file_list = [x.strip() for x in txt_file.readlines()]
-
-			print(f'[{self.MAIN_NAME}][LOG][OUT] File list found!')
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] File list found!')
 			txt_file.close()
 			return file_list
 
 		def dump_file_list(self):
-			print(f'[{self.MAIN_NAME}][LOG][IN] Dumping file list')
-
-			with open('file_lists/' + self.MAIN_NAME+'.txt', "w", encoding="utf-8") as txt_file:
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN] {inspect.stack()[0][3]}')
+			with open(f'./file_lists/{self.MAIN_TYPE}_{self.MAIN_NAME}.txt', "w", encoding="utf-8") as txt_file:
 				for i,line in enumerate(self.docs_list):
 					txt_file.write(line+'\n')
-
-			print(f'[{self.MAIN_NAME}][LOG][OUT] File list dumped')
-
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] File list dumped')
+		print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN] {inspect.stack()[0][3]}')
 		# dump_file_list(self)
 		main(self)
+		print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] {inspect.stack()[0][3]}')
 
 	def changed_files(self):
-		def main(self):		
-			# dump_LM_list(self)
+		def main(self):
 			diff_indexes = check_modified_files(self)
 			if diff_indexes:
 				self.download_files(diff_indexes)
@@ -143,89 +131,78 @@ class pars_rcoi(object):
 
 			 - returns: list of indexes
 			"""
-			print(f'[{self.MAIN_NAME}][LOG][IN] Checking modified files..')
-			prev_dict = get_prev_modified_files(self)
-			current_dict = self.docs_last_m
-
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN] {inspect.stack()[0][3]}')
+			prev_dict           = get_prev_modified_files(self)
+			current_dict        = self.docs_last_m
 			prev_dict 			= {name:date for name,date in prev_dict.items() if name in current_dict}
 			difference          = prev_dict.items() ^ current_dict.items()
 			names               = set([name for name,date in difference])
 			indexes             = [self.docs_og_names.index(name) for name in names]
-			
 			if difference:
-				print(f'[{self.MAIN_NAME}][LOG][OUT] Found modified files!')
+				print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] Found modified files!')
 			else:
-				print(f'[{self.MAIN_NAME}][LOG][OUT] No modified files found!')
-
+				print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] No modified files found!')
 			return indexes
 
 		def get_prev_modified_files(self):
-			"""
-			 - maybe "else" needed after checking if there's file
-			"""
-			print(f'[{self.MAIN_NAME}][LOG][IN] Checking previous "last modified" data')
-
-			# data = dict()
-			PATH = 'file_lists/' + self.MAIN_NAME + '_LM' + '.txt'
-
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN] {inspect.stack()[0][3]}')
+			PATH = f'./file_lists/{self.MAIN_TYPE}_{self.MAIN_NAME}_LM.txt'
 			if not os.path.exists(PATH):
-				print(f'[{self.MAIN_NAME}][LOG][INFO] "Last modified" data not found!')
-				print(f'[{self.MAIN_NAME}][LOG][INFO] Creating a "last modified" data..')
+				print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [INFO] "Last modified" data not found!')
+				print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [INFO] Creating a "last modified" data..')
 				dump_LM_list(self)
-
-			with open('file_lists/' + self.MAIN_NAME + '_LM' + '.txt', 'r') as text_file:
+			with open(PATH, "r") as text_file:
 				data = ast.literal_eval(text_file.read())
-
-			print(f'[{self.MAIN_NAME}][LOG][OUT] "Last modified" data found!')
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] "Last modified" data found!')
 			return data	
 
 		def dump_LM_list(self):
-			print(f'[{self.MAIN_NAME}][LOG][IN] Creating "last modified" data')
-
-			with open('file_lists/' + self.MAIN_NAME + '_LM' + '.txt', "w") as txt_file:
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN] {inspect.stack()[0][3]}')
+			with open(f'./file_lists/{self.MAIN_TYPE}_{self.MAIN_NAME}_LM.txt', "w") as txt_file:
 				data = str(self.docs_last_m)
 				txt_file.write(data)
-
-			print(f'[{self.MAIN_NAME}][LOG][OUT] "Last modified" data created!"')
-
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] "Last modified" data created!"')
+		print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN] {inspect.stack()[0][3]}')
 		# dump_LM_list(self)
 		main(self)
-	
+		print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [OUT] {inspect.stack()[0][3]}')
 
 	def download_files(self,indexes):
 		"""
-		- shortening to 45 characters is causing problems
-		  bc of identical file names.
 		- apparently there's a quotes-related problem with file name
 		  so I replace them with ''
 		"""
-		print(f'[{self.MAIN_NAME}][LOG][IN] Downloading new files..')
-
+		print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [IN] {inspect.stack()[0][3]}')
+		download_date = date.today()
+		self.last_dir = f'{download_date}_files'
+		if not os.path.exists(self.last_dir):
+			os.mkdir(self.last_dir)
 		for i in indexes:
-			print(f'[{self.MAIN_NAME}][LOG][INFO] Downloading file #{i+1}')
-
-			seed = uniform(1,4)
-			time.sleep(seed)
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [INFO] Downloading file #{i+1}')
+			time_wait = uniform(1,4)
+			time.sleep(time_wait)
 			response = requests.get(self.docs_links[i])
-
-			file_ext        = self.docs_og_names[i].split('.')[-1]
-			file_name_site_short  = str(self.docs_list[i][:45])
-			file_name       = str(randint(1,500)) + f'_'
-			file_name      += file_name_site_short + f'.' + file_ext
-			file_name       = file_name.replace(' ', '_')		
-			file_name 		= file_name.replace('"','')			
-
-			new_file = open(f'files/{self.MAIN_NAME}_{file_name}', 'wb')
+			seed = str(randint(1, 1000))
+			file_mask = f'{seed}_{self.MAIN_NAME}_{self.MAIN_TYPE}_'
+			file_name_full = file_mask+self.docs_list[i][:180].replace(' ', '_').replace('"','')
+			file_name_short = file_mask+self.docs_og_names[i].replace(' ', '_').replace('"','')
+			new_file = open(f'./{self.last_dir}/{file_name_short}', 'wb')
 			new_file.write(response.content)
 			new_file.close()
-			
-			self.bot_status += f'[{self.MAIN_NAME}]File {file_name} downloaded!\n'
-			print(f'[{self.MAIN_NAME}][LOG][INFO] File #{i+1} downloaded!')
-
+			bot_status_text = f'[{self.MAIN_NAME}][{self.MAIN_TYPE}]File {file_name_full} downloaded!\n' + \
+				'='*25
+			self.bot_status += bot_status_text
+			print(f'[{self.MAIN_NAME}] [{self.MAIN_TYPE}] [LOG] [INFO] File #{i+1} downloaded!')
 
 if __name__ == '__main__':
-	urls = ['http://rcoi.mcko.ru/organizers/info/gia-11/', 'http://rcoi.mcko.ru/organizers/info/gia-9/']
+	# pars_object = pars_rcoi('http://rcoi.mcko.ru/organizers/info/gia-11/')
+	# pars_object.get_info()
+	# t = vars(pars_object)
+	# print(t)
+	urls_organizers = ['http://rcoi.mcko.ru/organizers/info/gia-11/', 'http://rcoi.mcko.ru/organizers/info/gia-9/']
+	urls_methodolog = ['http://rcoi.mcko.ru/organizers/methodological-materials/ege/', 'http://rcoi.mcko.ru/organizers/methodological-materials/gia-9/']
+	urls = urls_organizers + urls_methodolog
 	for url in urls:
 		t = pars_rcoi(url)
 		t.get_info()
-
+		print(len(t.docs_list))
